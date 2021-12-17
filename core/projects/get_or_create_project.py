9f5476f603c000f3065bcad1c1739de18827e234
zeinlol <nickolaiborshchov@gmail.com>
2021-12-17 15:33:31 +0200

    fix typo

--- core/projects/get_or_create_project.py
+++ core/projects/get_or_create_project.py
@@ -5,7 +5,7 @@ from core.utils.randomizer import generate_random_name
 
 
 def get_project(sonar: SonarQubeClient, project_name: str = ''):
-    if project_name == '':
+    if not project_name:
         project_name = generate_random_name()
     print(f'Search for project: {project_name}')
 
@@ -13,6 +13,6 @@ def get_project(sonar: SonarQubeClient, project_name: str = ''):
     if not projects:
         print(f'Project "{project_name}" not found. Creating new project with this name...')
         projects = create_project(sonar=sonar, project=project_name, name=project_name)
-    target_project = projects[0]
+    target_project = projects[0]['project']
     print(f'Target project: {target_project["name"]}')
     return target_project
