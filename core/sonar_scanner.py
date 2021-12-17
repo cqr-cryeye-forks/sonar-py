302e2e7e78a0dc4d6519f6f92e0e722b7af5105b
zeinlol <nickolaiborshchov@gmail.com>
2021-12-17 18:34:37 +0200

    remove some exceptions from scanning

--- core/sonar_scanner.py
+++ core/sonar_scanner.py
@@ -1,9 +1,14 @@
+import os
 import subprocess
 
 from core import arguments
 
 
 def run_scanner(target_project: str, token: str, target_path: str):
+    # java_jdk = os.getenv('JAVA_HOME')
+    # if not java_jdk:
+    #     java_jdk = os.getenv('JDK_HOME')
+    # java_binaries = subprocess.run(['which', 'java'])
     try:
         output = subprocess.run(
             ['sonar-scanner',
@@ -11,6 +16,12 @@ def run_scanner(target_project: str, token: str, target_path: str):
              '-Dsonar.sources=.',
              f'-Dsonar.host.url={arguments.url}',
              f'-Dsonar.login={token}',
+             '-Dsonar.exclusions=**/*.java',
+             '-Dsonar.c.file.suffixes=-',
+             '-Dsonar.cpp.file.suffixes=-',
+             '-Dsonar.objc.file.suffixes=-',
+             # f'-Dsonar.java.jdkHome={java_jdk}',
+             # f'-Dsonar.java.binaries={java_binaries}',
              ],
             cwd=target_path,
         )
