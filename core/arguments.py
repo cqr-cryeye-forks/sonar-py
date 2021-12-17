e87f36d447de317e23fc3cb0ca20b422cfae5e4a
zeinlol <nickolaiborshchov@gmail.com>
2021-12-17 14:57:49 +0200

    update arguments

--- core/arguments.py
+++ core/arguments.py
@@ -5,16 +5,18 @@ from config.constants import sonarqube_url
 
 def create_parser():
     parser = argparse.ArgumentParser(description='Broken Minecraft seeds finder')
-    parser.add_argument('-u', '--url', type=str, nargs=1, default=sonarqube_url,
+    parser.add_argument('-u', '--url', type=str, default=sonarqube_url,
                         help=f'SonarQube link. Default is {sonarqube_url}')
-    parser.add_argument('-l', '--login', type=str,
+    parser.add_argument('-l', '--login', type=str, default=None,
                         help='SonarQube login')
-    parser.add_argument('-p', '--password', type=str,
+    parser.add_argument('-p', '--password', type=str, default=None,
                         help='SonarQube password')
-    parser.add_argument('-t', '--token', type=str,
+    parser.add_argument('-t', '--token', type=str, default=None,
                         help='SonarQube token')
-    parser.add_argument('-n', '--project', type=str, default='',
+    parser.add_argument('-n', '--project', type=str,  default=None,
                         help='Target SonarQube project')
+    parser.add_argument('-o', '--output', type=str,  default=None,
+                        help='Output file for data')
     parser.add_argument('-j', '--json', default=False, action='store_true',
                         help='Data as json')
     return parser.parse_args()
